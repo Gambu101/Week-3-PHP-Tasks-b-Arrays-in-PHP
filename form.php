e0ac59685c99088ba922f1b8aa68cf85e5d5df00
Gambu101 <gabriel.ohalete.244627@unn.edu.ng>
2022-10-14 21:45:25 +0100

    first commit

---
+++ form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="user_data.php" method="post">
+        <label for="name">Name:
+            <input type="text" name="name" id="name">
+        </label>
+        <label for="email">Email:
+            <input type="email" name="email" id="email">
+        </label><br>
+        <label for="dbo">Date of Birth:
+            <input type="date" name="date" id="date">
+        </label>
+        <label for="gender">Gender:
+            <input type="number" name="number" id="number">
+        </label>
+        <label for="country">Country:
+            <input type="text" name="country" id="country">
+        </label>
+    </form>
+</body>
+</html>
